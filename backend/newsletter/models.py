716fb7c14e594aa7857de9575e0039f18c5c4142
jatinbharadwaj <jitinrcks@gmail.com>
2021-03-10 15:18:12 +0530

    Setup Database model

--- backend/newsletter/models.py
+++ backend/newsletter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class newsletter(models.Model):
+    email = models.EmailField(max_length=254,unique=True)
+
+    def __str__(self):
+        return self.email
