da57ec6e5eb191cc4504a35962fa5c1a5d37f4fc
jatinbharadwaj <jitinrcks@gmail.com>
2021-03-10 15:39:04 +0530

    Create views and urls

--- backend/newsletter/views.py
+++ backend/newsletter/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import newsletterSerializer
+from .models import newsletter
 
-# Create your views here.
+
+class newsletterView(viewsets.ModelViewSet):
+    serializer_class = newsletterSerializer
+    queryset = newsletter.objects.all()
